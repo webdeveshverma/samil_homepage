295da5dd93b286c3b1966df8d3a1850811621f38
yeseol-lee <1999onell@gmail.com>
2020-10-20 21:16:12 +0900

    20/10/20

--- javascript/slide.js
+++ javascript/slide.js
@@ -1,16 +1,23 @@
-
-
 //슬라이드 5장 시작
+
+const content = document.querySelector("#js-text").innerHTML;
 const slide = document.querySelector('#slide-wrapper');
+
+
+
 
 var slideNum = 2;
 
 function toNextSlide() {
     
+    var cons = ["대구/경북지역 이사 전문업체!", "10년경력의 이사 베테랑이 이사를 직접 도와드립니다", "무료 간편 견적측정 가능! 전화상담/ 카톡 오픈상담/ 사진상담/ 홈페이지 상담!"];
+    
     if (slideNum > 3) {
         slideNum = 1;
     }
     slide.style.backgroundImage = `url(image/slide-${slideNum}.jpg)`;
+    content = cons[slideNum-1];
+    
     slideNum++;
 }
 
