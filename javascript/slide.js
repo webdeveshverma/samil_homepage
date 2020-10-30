62221033215e27668122a2ab33d4ec04a143b47c
yeseol-lee <1999onell@gmail.com>
2020-10-30 22:18:58 +0900

    fixed_slide_bug

--- javascript/slide.js
+++ javascript/slide.js
@@ -1,7 +1,6 @@
 //슬라이드 5장 시작
 
-const content = document.querySelector("#js-text").innerHTML;
-const slide = document.querySelector('#slide-wrapper');
+var slide = document.querySelector('#slide-wrapper');
 
 
 
@@ -10,13 +9,14 @@ var slideNum = 2;
 
 function toNextSlide() {
     
-    var cons = ["대구/경북지역 이사 전문업체!", "10년경력의 이사 베테랑이 이사를 직접 도와드립니다", "무료 간편 견적측정 가능! 전화상담/ 카톡 오픈상담/ 사진상담/ 홈페이지 상담!"];
+    var cons = ["<strong>대구/경북지역</strong> 이사 전문업체!", "<strong>10년경력</strong>의 이사 베테랑이 이사를 직접 도와드립니다", "무료 간편 견적측정 가능! <strong>전화상담/ 카톡 오픈상담/ 사진상담/ 홈페이지 상담!</strong>"];
     
     if (slideNum > 3) {
         slideNum = 1;
     }
     slide.style.backgroundImage = `url(image/slide-${slideNum}.jpg)`;
-    content = cons[slideNum-1];
+    
+    document.querySelector("#js-text").innerHTML = cons[slideNum-1];
     
     slideNum++;
 }
